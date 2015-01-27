9b1467ccc557247c22f58e8cf6d9aa6b09f4cbe8
Scott Merkling <sbmerkling@gmail.com>
2015-01-27 15:10:57 +0700

    Moved to a more object oriented architecture.

--- js/gol.js
+++ js/gol.js
@@ -1,66 +1,151 @@
-/*
-A Game of Life implementation.  This script expects there to be a containing element
-with id "container".  It displays the game as a 100x100 grid of divs, each with 
-class="cell", and class="live" if alive.
-*/
+function Grid(w, h) {
+	var grid = this;
+	grid.width = w;
+	grid.height = h;
+	grid.rows = [];
 
-var $container;
-var size_px = {'width': null, 'height': null};
-var num_cells = {'width': 20, 'height': 20};
-var cell_size = {'width': null, 'height': null};
-var gutter_width = 1;
-var cells;
-
-function create_cells() {
-	cells = [];
-	cell_size.width = (size_px.width - gutter_width*(num_cells.width-1)) / num_cells.width;
-	cell_size.height = (size_px.height - gutter_width*(num_cells.height-1)) / num_cells.height;
-	for (var i=0; i<num_cells.height; i++) {
-		var curr_y = i*cell_size.height + i*gutter_width;
-		var curr_row = $('<tr></tr>');
-		cells.push([]);
-		for (var j=0; j<num_cells.width; j++) {
-			var curr_x = j*cell_size.width + j*gutter_width;
-			var cell = $('<td class="cell"></td>').attr({
-				'width': cell_size.width,
-				'height': cell_size.height,
-			}).click(function() {
-				$(this).toggleClass('live');
-
-			});
-			cells[i].push({
-				'html_cell': cell,
-				'live': false
-			});
-			curr_row.append(cell);
+	grid.reset = function() {
+		var x, 
+			y, 
+			row;
+		grid.rows = [];
+		for (x=0; x<grid.width; x++) {
+			row = [];
+			for (y=0; y<grid.height; y++) {
+				row.push(new Cell(x, y));
+			}
+			grid.rows.push(row);
 		}
-		$container.append(curr_row);
 	}
-}
 
-function count_live_neighbors(cells, x, y) {
-	var cell = cells[x][y];
-	var live_neighbors = 0;
-	for (var i=-1; i<2; i++) {
-		for (var j=-1; j<2; j++) {
-			if (x+i > 0 && y+j > 0) {
-				if (cells[x+i][y+j]['live']) {
+	grid.reset();
 
-				}
+	grid.traverse = function(fn) {
+		var x, y;
+		for (x=0; x<grid.width; x++) {
+			for (y=0; y<grid.height; y++) {
+				fn(grid.rows[x][y]);
 			}
 		}
 	}
+
+	grid.step = function() {
+		// Determine who will live and die next run
+		grid.traverse(function(cell) {
+			cell.compute_live_neighbors();
+		});
+		grid.traverse(function(cell) {
+			cell.update();
+		});
+	}
+	
+	function Cell(x, y) {
+		var cell = this;
+		cell.x = x;
+		cell.y = y;
+		cell.live = false;
+		cell.live_neighbors = 0;
+		cell.element = null;
+	}
+
+	Cell.prototype.compute_live_neighbors = function() {
+		var x, 
+			y, 
+			live_neighbors = 0
+			cell = this;
+		for (x=Math.max(cell.x-1, 0); x<=Math.min(cell.x+1, grid.width-1); x++) {
+			for (y=Math.max(cell.y-1, 0); y<=Math.min(cell.y+1, grid.height-1); y++) {
+				var curr_cell = grid.rows[x][y];
+				if (curr_cell && curr_cell !== cell && curr_cell.live) {
+					live_neighbors += 1;
+				}
+			}
+		}
+		cell.live_neighbors = live_neighbors;
+	}
+
+	Cell.prototype.update = function() {
+		var cell = this;
+		if (cell.live) {
+			if (cell.live_neighbors !== 2 || cell.live_neighbors !== 3) {
+				cell.live = false;
+			}
+		} else if (cell.live_neighbors === 3) {
+			cell.live = true;
+		}
+	}
+
+	Cell.prototype.toggle = function() {
+		var cell = this;
+		cell.live = !cell.live;
+	}
 }
 
-function do_tick() {
+function View(grid) {
+	var view = this;
+	view.table = document.getElementById('game_table');
+	view.grid = grid;
 
+	view.init = function() {
+		var x, y, tr, td;
+		for (x=0; x<view.grid.width; x++) {
+			tr = document.createElement('tr');
+			for (y=0; y<view.grid.height; y++) {
+				td = document.createElement('td');
+				addClass(td, 'cell');
+				cell = view.grid.rows[x][y];
+				cell.element = td;
+				td.addEventListener('click', (function(cell) {
+					return function(e) {
+						cell.toggle();
+						view.update();
+					}
+				})(cell));
+
+				tr.appendChild(td);
+			}
+			view.table.appendChild(tr);
+		}
+	}
+
+	view.update = function() {
+		grid.traverse(function(cell) {
+			if (cell.live) {
+				addClass(cell.element, 'live');
+			} else {
+				removeClass(cell.element, 'live');
+			}
+		});
+	}
+
+	view.animate = function() {
+		view.grid.step();
+		view.update();
+	}
+	setInterval((function(view) {
+		return function() {
+			view.animate();
+		}
+	})(view), 2000);
+
+	view.init();
 }
 
-$(document).ready(function() {
-	$container = $('#container');
-	size_px.width = $container.width();
-	size_px.height = $container.height();
+function GameOfLife() {
+	this.grid = new Grid(20, 20);
+	this.view = new View(this.grid);
+}
 
-	create_cells();
-	// setInterval(do_tick, 500);
-});
+function addClass(el, className) {
+    var classRegexp = new RegExp('\\b' + className + '\\b', 'g');
+    if (!classRegexp.test(el.className)) {
+        el.className += ' ' + className;
+    }
+}
+
+function removeClass(el, className) {
+    var classRegexp = new RegExp('\\b' + className + '\\b', 'g');
+    el.className = el.className.replace(classRegexp, '');
+}
+
+var gol = new GameOfLife();
