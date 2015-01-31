543a1655ac25169ccf74a54145017f035c9f37ca
Scott Merkling <sbmerkling@gmail.com>
2015-01-31 14:30:42 +0700

    The grid is now displayed as Mona Lisa. Fixed the stop button and clicking on cells.

--- js/gol.js
+++ js/gol.js
@@ -1,3 +1,24 @@
+/*
+TODO: Fix stop button and clicking on cells DONE
+Put the mona lisa in? DONE
+-- Insert a formation wherever you want
+-- Separate view and model data more
+*/
+var game_of_life;
+var canvas = document.createElement("canvas");
+canvas.style.display = "none";
+document.getElementById('container').appendChild(canvas);
+var ctx = canvas.getContext('2d');
+var img = new Image();
+img.addEventListener("load", function() {
+	canvas.width = img.width;
+	canvas.height = img.height;
+  	ctx.drawImage(img, 0, 0);
+  	game_of_life = new GameOfLife();
+}, false);
+img.src = 'img/mona_lisa.jpg';
+
+
 function Grid(w, h) {
 	var grid = this;
 	grid.width = w;
@@ -33,18 +54,74 @@ function Grid(w, h) {
 		/*
 			Try and map x position to red, and y position to blue.
 		*/
-		var x_mult = x / grid.width,
-			y_mult = y / grid.height,
-			green = '99',
-			red =  Math.floor(256 * x_mult).toString(16),
-			blue = Math.floor(256 * y_mult).toString(16);
-		while (red.length < 2) {
-			red = "0" + red;
+		// var x_mult = x / grid.width,
+		// 	y_mult = y / grid.height,
+		// 	green = '99',
+		// 	red =  Math.floor(256 * x_mult).toString(16),
+		// 	blue = Math.floor(256 * y_mult).toString(16);
+		// while (red.length < 2) {
+		// 	red = "0" + red;
+		// }
+		// while (blue.length < 2) {
+		// 	blue = "0" + blue;
+		// }
+		// return '#' + red + green + blue;
+
+		function avg_color_of_img_range(x, y, width, height) {
+			var color_data = ctx.getImageData(x, y, width, height).data;
+			var rgb = {r: 0, g: 0, b: 0};
+			var block_size = 4;
+			var i;
+			var count = 0;
+			// console.log(x + "  " + y + "  " + width + "  " + height);
+			// console.log(color_data);
+			for (i=0; i<color_data.length; i+=block_size) {
+				rgb.r += color_data[i];
+				rgb.g += color_data[i+1];
+				rgb.b += color_data[i+2];
+				count ++;
+			}
+			rgb.r = Math.floor(rgb.r / count).toString(16);
+			rgb.g = Math.floor(rgb.g / count).toString(16);
+			rgb.b = Math.floor(rgb.b / count).toString(16);
+			while (rgb.r.length < 2) {
+				rgb.r = "0" + rgb.r;
+			}
+			while (rgb.g.length < 2) {
+				rgb.g = "0" + rgb.g;
+			}
+			while (rgb.b.length < 2) {
+				rgb.b = "0" + rgb.b;
+			}
+			return "#" + rgb.r + rgb.g + rgb.b;
 		}
-		while (blue.length < 2) {
-			blue = "0" + blue;
-		}
-		return '#' + red + green + blue;
+		var img_cell_width = Math.floor(img.width / grid.width);
+		var img_cell_height = Math.floor(img.height / grid.height);
+		var img_start_x = img_cell_width*x,
+			img_start_y = img_cell_height*y;
+		// console.log("Cell_x and cell_Y: (" + x + ", " + y + ").  Imgx and imgy: (" + img_x + ", " + img_y + ")");
+		return avg_color_of_img_range(img_start_x, img_start_y, img_cell_width, img_cell_height);
+
+		// function color(x, y) {
+		// 	return canvas.getContext('2d').getImageData(x, y, 1, 1).data;
+		// }
+		// return "red";
+		/*
+		try {
+		        data = context.getImageData(0, 0, width, height);
+		    } catch(e) {
+		        // security error, img on diff domain 
+		        return defaultRGB;
+		    }
+
+		    length = data.data.length;
+
+		    while ( (i += blockSize * 4) < length ) {
+		        ++count;
+		        rgb.r += data.data[i];
+		        rgb.g += data.data[i+1];
+		        rgb.b += data.data[i+2];
+		    }*/
 	}
 
 	grid.step = function() {
@@ -62,6 +139,12 @@ function Grid(w, h) {
 		// Todo: have this use set_config
 		grid.traverse(function(cell) {
 			cell.update_liveliness(Math.random() > .5);
+		});
+	}
+
+	grid.full_config = function() {
+		grid.traverse(function(cell) {
+			cell.update_liveliness(true);
 		});
 	}
 
@@ -126,11 +209,11 @@ function Grid(w, h) {
 	Cell.prototype.update_liveliness = function(live) {
 		var cell = this;
 		cell.live = live;
-		if (live) {
-			cell.element.style.backgroundColor = cell.live_color;
-		} else {
-			cell.element.style.backgroundColor = "#ffffff";
-		}
+		// if (live) {
+		// 	cell.element.style.backgroundColor = cell.live_color;
+		// } else {
+		// 	cell.element.style.backgroundColor = "#ffffff";
+		// }
 	}
 
 	Cell.prototype.toggle = function() {
@@ -148,9 +231,9 @@ function View(grid) {
 
 	view.init = function() {
 		var x, y, tr, td, cell_width, cell_height;
-		for (x=0; x<view.grid.width; x++) {
+		for (y=0; y<view.grid.height; y++) {
 			tr = document.createElement('tr');
-			for (y=0; y<view.grid.height; y++) {
+			for (x=0; x<view.grid.width; x++) {
 				td = document.createElement('td');
 				addClass(td, 'cell');
 				cell = view.grid.rows[x][y];
@@ -172,18 +255,19 @@ function View(grid) {
 	view.update = function() {
 		grid.traverse(function(cell) {
 			if (cell.live) {
-				// addClass(cell.element, 'live');
 				removeClass(cell.element, 'dead');
-
+				cell.element.style.backgroundColor = cell.live_color;
 			} else {
-				// removeClass(cell.element, 'live');
 				addClass(cell.element, 'dead');
-
+				cell.element.style.backgroundColor = "#ffffff";
 			}
 		});
 	}
 
 	view.animate = function() {
+		if (!view.running) {
+			return;
+		}
 		var tick_frequency = 3000 / view.speed.value;
 		if (tick_frequency !== Infinity) {
 			view.grid.step();
@@ -213,7 +297,7 @@ function View(grid) {
 
 function GameOfLife() {
 	var gol = this;
-	gol.grid = new Grid(55, 55);
+	gol.grid = new Grid(79, 120);
 	gol.view = new View(this.grid);
 
 	gol.randomize_config = function() {
@@ -224,6 +308,11 @@ function GameOfLife() {
 	gol.set_config = function(config, start_x, start_y) {
 		// Can I do something cleaner here than have start_x and start_y?
 		gol.grid.set_config(config, start_x, start_y);
+		gol.view.update();
+	}
+	gol.full_config = function() {
+		// TODO: Get rid of this and put it in gol.set_config
+		gol.grid.full_config();
 		gol.view.update();
 	}
 }
@@ -324,10 +413,6 @@ function Configuration(c_type) {
 	}
 }
 
-var game_of_life = new GameOfLife();
-
-
-
 // shim layer with setTimeout fallback
 window.requestAnimationFrame = (function(){
   return  window.requestAnimationFrame       ||
@@ -337,3 +422,4 @@ window.requestAnimationFrame = (function(){
             window.setTimeout(callback, 1000 / 60);
           };
 })();
+
