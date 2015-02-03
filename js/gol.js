4a61a1a27ac18e378491faba2987a06e0cd3b2d1
Scott Merkling <sbmerkling@gmail.com>
2015-02-03 15:03:40 +0700

    Adding a preview function so a user can put a configuration anywhere on the game table

--- js/gol.js
+++ js/gol.js
@@ -1,6 +1,8 @@
 /*
 -- Insert a formation wherever you want
--- Separate view and model data more
+a) Hover over table cell and have it filled in
+b) Hover over table cell and have a formation appear with start_x, and start_y from cursor
+c) Click to plcae that
 */
 var game_of_life;
 var canvas = document.createElement("canvas");
@@ -22,6 +24,7 @@ function Grid(w, h) {
 	grid.width = w;
 	grid.height = h;
 	grid.rows = [];
+	grid.previewed_cells = [];
 
 	grid.reset = function() {
 		var x, 
@@ -38,10 +41,22 @@ function Grid(w, h) {
 	}
 	grid.reset();
 
-	grid.traverse = function(fn) {
+	grid.traverse = function(fn, x1, y1, x2, y2) {
 		var x, y;
-		for (x=0; x<grid.width; x++) {
-			for (y=0; y<grid.height; y++) {
+		if (!x1) {
+			x1 = 0;
+		}
+		if (!x2) {
+			x2 = view.grid.width;
+		}
+		if (!y1) {
+			y1 = 0;
+		}
+		if (!y2) {
+			y2 = view.grid.height;
+		}
+		for (x=x1; x<x2; x++) {
+			for (y=y1; y<y2; y++) {
 				fn(grid.rows[x][y]);
 			}
 		}
@@ -95,6 +110,35 @@ function Grid(w, h) {
 			cell.update();
 		});
 	}
+
+	grid.preview_config = function(config, start_x, start_y) {
+		var map, x, y, cell;
+		grid.remove_previews();
+		if (!start_x) {
+			start_x = 0;
+		}
+		if (!start_y) {
+			start_y = 0;
+		}
+		map = config.map;
+		for (x=start_x; x<start_x+map[0].length+1; x++) {
+			for (y=start_y; y<start_y+map.length+1; y++) {
+				cell = grid.rows[x][y];
+				if (map[x-start_x] && map[x-start_x][y-start_y]) {
+					cell.previewed_live = true;
+					grid.previewed_cells.push(cell);
+				}
+			}
+		}
+	}
+
+	grid.remove_previews = function() {
+		var i;
+		for (i=0; i<grid.previewed_cells.length; i++) {
+			grid.previewed_cells[i].previewed_live = false;
+		}
+		grid.previewed_cells = [];
+	}
 	
 	grid.set_config = function(config, start_x, start_y) {
 		if (config === 'random') {
@@ -136,9 +180,12 @@ function Grid(w, h) {
 		cell.x = x;
 		cell.y = y;
 		cell.live = false;
+		cell.previewed_live = false;
 		cell.live_neighbors = 0;
 		cell.element = null;
-		cell.live_color = "#ffffff";
+		cell.live_color = "red";	// This gets set later, but red is the fallback
+		cell.dead_color = "#ffffff";
+		cell.previewed_live_color = "blue";
 	}
 
 	Cell.prototype.compute_live_neighbors = function() {
@@ -181,6 +228,27 @@ function View(grid) {
 	view.running = false;
 	view.grid = grid;
 
+	
+	view.table.addEventListener('mouseover', function(evt) {
+		var cell,
+			x,
+			y,
+			td = document.elementFromPoint(evt.clientX, evt.clientY);
+		for (x=0; x<view.grid.width && !cell; x++) {
+			for (y=0; y<view.grid.height && !cell; y++) {
+				if (view.grid.rows[x][y].element === td) {
+					cell = view.grid.rows[x][y];
+				}
+			}
+		}
+		view.grid.preview_config(new Configuration('block'), cell.x, cell.y);
+		view.update();		// TODO: Update affected section only
+	});
+	view.table.addEventListener('mouseleave', function(evt) {
+		view.grid.remove_previews();
+		view.update();	// TODO: Update affected section only
+	});
+
 	view.init = function() {
 		var x, y, tr, td, cell_width, cell_height;
 		for (y=0; y<view.grid.height; y++) {
@@ -204,16 +272,19 @@ function View(grid) {
 		}
 	}
 
-	view.update = function() {
+	view.update = function(x1, y1, x2, y2) {
+
 		grid.traverse(function(cell) {
 			if (cell.live) {
 				removeClass(cell.element, 'dead');
 				cell.element.style.backgroundColor = cell.live_color;
+			} else if (cell.previewed_live) {
+				cell.element.style.backgroundColor = cell.previewed_live_color;
 			} else {
 				addClass(cell.element, 'dead');
-				cell.element.style.backgroundColor = "#ffffff";
+				cell.element.style.backgroundColor = cell.dead_color;
 			}
-		});
+		}, x1, y1, x2, y2);
 	}
 
 	view.animate = function() {
@@ -250,7 +321,7 @@ function View(grid) {
 
 function GameOfLife() {
 	var gol = this;
-	gol.grid = new Grid(79*2, 120*2);
+	gol.grid = new Grid(79, 120);
 	gol.view = new View(this.grid);
 
 	gol.set_config = function(config, start_x, start_y) {
